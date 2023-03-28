0116e5412fffecf805e71cb8ca6a4b8594fbaf5a
TomBTD-OC <tombutaud83136@gmail.com>
2023-03-28 13:00:24 +0200

    add filters

--- FrontEnd/assets/scripts.js
+++ FrontEnd/assets/scripts.js
@@ -1,7 +1,7 @@
 
                     //appel de l'API avec fetch//
-loadJSON('http://localhost:5678/api/works');
-async function loadJSON(fname) {
+load_gallery('http://localhost:5678/api/works');
+async function load_gallery(fname) {
     var response = await fetch(fname)
     var data =  await response.json()
                     // création de la loop pour l'affichage de la gallery//
@@ -10,8 +10,20 @@ async function loadJSON(fname) {
         let figure = document.createElement("figure");
         figure.innerHTML =  "<img src=" + data[i].imageUrl  + "> <figcaption>" + data[i].title + "</figcaption>";
         gallery.append(figure);
-
     }
-    
-}
+ }
 
+ load_categories('http://localhost:5678/api/categories');
+ async function load_categories(fname) {
+     var response = await fetch(fname)
+     var data =  await response.json()
+ 
+     filter = document.querySelector(".filtres");
+     for (j = 0; j < data.length; j++) {
+         let button = document.createElement("button");
+         button.id = data[j].id;
+         button.classList.add("filter_button");
+         button.appendChild(document.createTextNode(data[j].name));  
+         filter.append(button);
+     }     
+      }
